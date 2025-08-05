27a43ab27c1b453ea4cfac7bd5b4c116345538a0
I Nyoman Pastika <inyomanpastika@Is-MacBook-Air.local>
2025-08-05 12:55:54 +0800

    update penulsian import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import React, { useEffect } from 'react';
-import './app.css';
+import './App.css';
 
 import Map from './pages/map';
 import Home from './pages/Home';
